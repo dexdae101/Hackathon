32463b5c44e3a4d1e1f6407d5c31cf1fd6e4b90c
dexdae101 <davide.casarin24@gmail.com>
2024-08-13 16:05:49 +0200

    deleted-old-scene-generation?

--- src/player.gd
+++ src/player.gd
@@ -1,31 +1,23 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+const ROT_SPEED = deg_to_rad(40)
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("forward"):
+		direction.z = -1 * SPEED
+	if Input.is_action_pressed("backwards"):
+		direction.z = 1 * SPEED
+	if Input.is_action_pressed("left"):
+		rotation.y += ROT_SPEED*delta
+	if Input.is_action_pressed("right"):
+		rotation.y -= ROT_SPEED*delta
+	direction = direction.rotated(Vector3.UP, rotation.y)
+	velocity = direction
 	move_and_slide()
+
+func round_excess(n: float):
+	if n - int(n) >0: return n+1
+	else: return n
